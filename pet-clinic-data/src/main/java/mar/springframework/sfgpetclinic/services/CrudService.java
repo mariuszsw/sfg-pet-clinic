6ed30f907e0f4578f7d4d0d9c7d09878a5cedba8
Mariusz Świerkot <mariusz.swierkot@remotecraftsmen.com>
2021-07-30 16:48:42 +0200

    Implements Map based Services, closes #6

--- pet-clinic-data/src/main/java/mar/springframework/sfgpetclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/mar/springframework/sfgpetclinic/services/CrudService.java
@@ -3,13 +3,15 @@ package mar.springframework.sfgpetclinic.services;
 import java.util.Set;
 
 public interface CrudService<T, ID> {
+    Set<T> findAll();
+
     T findById(ID id);
 
     T save(T object);
 
     void delete(T object);
 
-    void  deleteById();
+    void  deleteById(ID id);
 
 }
 
