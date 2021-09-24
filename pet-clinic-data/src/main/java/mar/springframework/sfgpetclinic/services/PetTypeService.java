ffa89f81883e5a05c8c18ff2a417e61d24bd5600
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2021-09-24 21:28:11 +0200

    added PetTypeService, closes #30

---
+++ pet-clinic-data/src/main/java/mar/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package mar.springframework.sfgpetclinic.services;
+
+import mar.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
