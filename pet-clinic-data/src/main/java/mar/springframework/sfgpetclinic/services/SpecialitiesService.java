263c18bc320f3840c48d0db24b05c4c5cc8e6668
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2021-09-25 04:33:22 +0200

    added Specialties Map Impl, closes #44

---
+++ pet-clinic-data/src/main/java/mar/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,9 @@
+package mar.springframework.sfgpetclinic.services;
+
+import mar.springframework.sfgpetclinic.model.Speciality;
+
+import java.util.Set;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long>{
+
+}
